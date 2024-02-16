29cd9f726cec7f2b28732ed4cf541a73bd1f8280
Phong Ngo <phong.trieungo@hotmail.com>
2024-02-16 08:20:05 +0700

    Implement room participant list and toolbar

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,50 @@
+'use client'
+
+import { useOthers, useSelf } from "@/liveblocks.config"
+
 import { Skeleton } from "@/components/ui/skeleton"
+import UserAvatar from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_USER = 2;
 
 export default function Participant() {
+  const users = useOthers();
+  const myself = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USER;
+
   return (
     <section className="absolute h-12 top-2 right-2 bg-white rounded-md px-1.5 flex items-center shadow-md">
-      Participants
+      <section className="flex gap-x-2">
+        { 
+          users.slice(0, MAX_SHOWN_USER).map(user => (
+            <UserAvatar 
+              borderColor={connectionIdToColor(user.connectionId)}
+              key={user.connectionId} 
+              src={user.info?.picture} 
+              name={user.info?.name}
+              fallback={user.info?.name || 'T'} />
+          ))
+        }
+        {
+          myself && (
+            <UserAvatar 
+              borderColor={connectionIdToColor(myself.connectionId)}
+              src={myself.info?.picture} 
+              name={`${myself.info?.name} (You)`}
+              fallback={myself.info?.name || 'M'} />
+          )
+        }
+
+        {
+          hasMoreUsers && (
+            <UserAvatar 
+              name={`${users.length - MAX_SHOWN_USER} more`}
+              fallback={`+${users.length - MAX_SHOWN_USER}`}
+            />
+          )
+        }
+      </section>
     </section>
   )
 }
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,16 +1,22 @@
 import { Skeleton } from "@/components/ui/skeleton"
+import ToolButton from "./tool-button"
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react"
 
 export default function Toolbar() {
   return (
     <section className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 ">
       <ul className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md list-none">
-        <li>Pencil</li>
-        <li>Square</li>
+        <ToolButton label="Select" icon={MousePointer2} onClick={() => {}} isActive={false} />
+        <ToolButton label="Text" icon={Type} onClick={() => {}} isActive={false} />
+        <ToolButton label="Sticky Note" icon={StickyNote} onClick={() => {}} isActive={false} />
+        <ToolButton label="Rectangle" icon={Square} onClick={() => {}} isActive={false} />
+        <ToolButton label="Ellipse" icon={Circle} onClick={() => {}} isActive={false} />
+        <ToolButton label="Pen" icon={Pencil} onClick={() => {}} isActive={false} />
       </ul>
 
       <ul className="bg-white list-none rounded-md p-1.5 flex flex-col items-center shadow-md ">
-        <li>Undo</li>
-        <li>Redo</li>
+        <ToolButton label="Undo" icon={Undo2} onClick={() => {}} isDisabled={true} />
+        <ToolButton label="Redo" icon={Redo2} onClick={() => {}} isDisabled={true} />
       </ul>
     </section>
   )
