36820b2710137da7046f94f33b682565d8061fad
Phong Ngo <phong.trieungo@hotmail.com>
2024-02-17 13:10:50 +0700

    Detect and display cursor per user

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import Info from "./info"
 import Participant from "./participants"
 import Toolbar from "./toolbar"
-import { CanvasMode, CanvasState } from "@/types/canvas"
-import { useCanRedo, useCanUndo, useHistory, useRedo } from "@/liveblocks.config"
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas"
+import { useCanRedo, useCanUndo, useHistory, useMutation } from "@/liveblocks.config"
+import CursorsPresence from "./cursors-presence"
+import { pointerEventToCanvasPointer } from "@/lib/utils"
 
 interface CanvasProps {
 	boardId: string;
@@ -16,9 +18,30 @@ export default function Canvas({ boardId }: CanvasProps) {
     mode: CanvasMode.None
   });
 
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
   const history = useHistory();
   const canRedo = useCanRedo();
   const canUndo = useCanUndo();
+
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY
+    }))
+  }, []);
+
+  const onPointerMove = useMutation(({setMyPresence}, e: React.PointerEvent) => {
+    e.preventDefault();
+    const current = pointerEventToCanvasPointer(e, camera);
+    setMyPresence({ cursor: current })
+  }, []);
+
+  const onPointerLeave = useMutation((
+    { setMyPresence }
+  ) => {
+    setMyPresence({ cursor: null })
+  }, []);
 
 	return (
 		<main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -32,6 +55,15 @@ export default function Canvas({ boardId }: CanvasProps) {
         redo={history.redo}
         undo={history.undo}
         />
+      <svg 
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}>
+        <g>
+          <CursorsPresence />
+        </g>
+      </svg>
 		</main>
 	)
 }
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,14 +2,15 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
   
 const client = createClient({
-  authEndpoint: '/api/liveblocks-auth'
+  authEndpoint: '/api/liveblocks-auth',
+  throttle: 16
 });
 
 // Presence represents the properties that exist on every user in the Room
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  // cursor: { x: number, y: number } | null,
+  cursor: { x: number, y: number } | null,
   // ...
 };
 
